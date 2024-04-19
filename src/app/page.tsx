4930806154054cc752cd16922a9a22a39f667caf
yagodev <yagodev.contato@gmail.com>
2024-04-18 23:14:49 -0300

    Feat: Starting create the design money balance dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,41 @@
+import { DollarSign } from "lucide-react";
+
 export default function Home() {
   return (
-    <div>
-      <header>Header</header>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+      <aside className="px-4 py-2">
         <div>
-          <h1>Boa noite, Yago Rodrigues Freitas</h1>
-          <p>Seu balanço do mês está em R$ 2.399,75</p>
-        </div>
-        <aside>
+          <h4>Money balance</h4>
           <ul>
             <li>Dashboard</li>
             <li>Balance</li>
           </ul>
-        </aside>
+        </div>
+      </aside>
+
+      <main>
+        <div>
+          <h1 className="text-4xl font-bold">Money balance</h1>
+          <h3 className="text-lg text-zinc-400">Boa noite, Yago Rodrigues Freitas</h3>
+        </div>
+
+        <section>
+          <div className="grid gap-4 mt-12">
+            <div className="rounded-xl border bg-card text-card-foreground shadow">
+              <div className="p-6 flex flex-row items-center justify-between space-y-0 pb-2">
+                <h3 className="tracking-tight text-sm font-medium">Salário</h3>
+                <DollarSign />
+              </div>
+
+              <div className="p-6 pt-0">
+                <div>
+                  <div>R$ 4.200,00</div>
+                  <p>+21.5% no último ano</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
